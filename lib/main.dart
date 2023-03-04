188f41c48dea781e3a56f9e6700e421681daa87c
agungfir98 <agungfir98@gmail.com>
2023-03-04 11:58:29 +0700

    split code per pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'pages/collection.dart';
+
+import './pages/collection.dart';
+import './pages/home_page.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -20,162 +22,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: MyHomePage.nameRoute,
       routes: {
-        '/': (context) => const MyHomePage(title: 'Poke Not GO'),
-        '/collections': (context) => const PokeCollection()
-      },
-      // home: const MyHomePage(title: 'Poke Not GO'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<dynamic> _pokemonList = [];
-  String searchInput = "";
-  @override
-  void initState() {
-    super.initState();
-    initialization();
-    fetchData();
-  }
-
-  void fetchData() async {
-    HttpLink apiUri = HttpLink('https://graphql-pokeapi.graphcdn.app/');
-    GraphQLClient qlClient = GraphQLClient(
-      link: apiUri,
-      cache: GraphQLCache(
-        store: HiveStore(),
-      ),
-    );
-
-    String qlString = """query (\$limit: Int!, \$offset: Int!) {
-    pokemons(limit: \$limit, offset: \$offset) {
-      results {
-        name
-        id
-        dreamworld
-        image
-      }
-    }
-  }""";
-
-    QueryResult queryResult = await qlClient.query(QueryOptions(
-        document: gql(qlString), variables: const {'limit': 5, 'offset': 1}));
-
-    setState(() {
-      _pokemonList = queryResult.data!["pokemons"]!["results"];
-    });
-  }
-
-  void initialization() async {
-    FlutterNativeSplash.remove();
-  }
-
-  void submitSearch() {
-    print(searchInput);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                padding: const EdgeInsetsDirectional.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                alignment: Alignment.center,
-                width: 300,
-                child: TextFormField(
-                  onChanged: (value) => setState(() {
-                    searchInput = value;
-                  }),
-                  decoration: InputDecoration(
-                    hintText: 'Search...',
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.search),
-                      color: Colors.black26,
-                      onPressed: submitSearch,
-                    ),
-                  ),
-                ),
-              ),
+        MyHomePage.nameRoute: (context) => const MyHomePage(
+              title: 'Poke Not GO',
             ),
-            GridView.builder(
-                itemCount: _pokemonList.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                shrinkWrap: true,
-                padding: const EdgeInsets.all(8.0),
-                itemBuilder: (_, index) {
-                  return pokeCard(
-                    _pokemonList[index]!['image'],
-                    _pokemonList[index]!['name'],
-                  );
-                }),
-            ElevatedButton(
-              onPressed: () {
-                print(_pokemonList);
-              },
-              child: const Text('console'),
-            ),
-          ],
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/collections');
-              },
-              tooltip: 'Pokemon ku',
-              child: const Icon(
-                Icons.catching_pokemon,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
-        ));
-  }
-
-  Card pokeCard(String imgUrl, String pokeName) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        side: const BorderSide(
-          color: Color.fromARGB(255, 208, 208, 208),
-        ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        children: [
-          Image(
-            fit: BoxFit.cover,
-            height: 100,
-            image: NetworkImage(imgUrl),
-          ),
-          const SizedBox(),
-          Text(pokeName)
-        ],
-      ),
+        PokeCollection.nameRoute: (context) => const PokeCollection()
+      }, // home: const MyHomePage(title: 'Poke Not GO'),
     );
   }
 }
